70b412fee55d4f7608220a47ddc74f26d6c5d9d0
Ismael-Rvas <ismael.rivas.developer@gmail.com>
2025-02-28 20:29:53 +0100

    Añadir estilos, y rutas nuevas

--- src/app/page.js
+++ src/app/page.js
@@ -2,37 +2,30 @@ import Link from "next/link";
 
 export default async function Home() {
   return (
-    <>
-      <h1 className="text-3xl font-bold">Página principal 🍕</h1>
+    <div className="max-w-4xl mx-auto mt-24 p-4 space-y-8">
+      <h1 className="text-4xl font-extrabold mb-6 text-center border-b border-gray-200">HOME</h1>
 
-      <div className="flex flex-col items-start">
-        <Link href="/admin" className="text-blue-700">
-          Panel de admin
+      <div className="space-y-4">
+
+        <h2 className="text-2xl font-bold">Datos Usuario:</h2>
+        <Link href="/dashboard" className="block p-4 rounded-lg bg-gray-100 hover:bg-gray-200">
+          Dashboard del Usuario
         </Link>
-        <Link href="/dashboard" className="text-blue-700">
-          Dashboard del usuario
-        </Link>
-        <Link href="/productos" className="text-blue-700">
-          Productos
-        </Link>
-        <Link href="/proveedores" className="text-blue-700">
-          Proveedores
-        </Link>
-        <Link href="/about" className="text-blue-700">
+        <Link href="/about" className="block p-4 rounded-lg bg-gray-100 hover:bg-gray-200">
           About
         </Link>
-        <Link href="/repartidores" className="block text-2xl font-bold">
-        REPARTIDORES
+        <h2 className="text-2xl font-bold">Tablas Pizzeria:</h2>
+        <Link href="/repartidores" className="block p-4 rounded-lg bg-blue-500 text-white hover:bg-blue-600">
+          Repartidores
         </Link>
-        <Link href="/pedidos" className="block text-2xl font-bold">
-        PEDIDOS
+        <Link href="/pedidos" className="block p-4 rounded-lg bg-blue-500 text-white hover:bg-blue-600">
+          Pedidos
         </Link>
-        <Link href="/pizzas" className="block text-2xl font-bold">
-        PIZZAS
+        <Link href="/pizzas" className="block p-4 rounded-lg bg-blue-500 text-white hover:bg-blue-600">
+          Pizzas
         </Link>
       </div>
-
-    </>
+    </div>
   )
 }
 
--- src/components/header.js
+++ src/components/header.js
@@ -9,24 +9,24 @@ async function Header() {
     // console.log(session);
 
     return (
-        <header className='bg-blue-700 text-white flex px-10 py-2 justify-between items-center'>
-            <nav className='flex gap-4 items-center'>
+        <header className="bg-blue-700 text-white flex px-10 py-2 justify-between items-center shadow-md">
+            <nav className="flex gap-4 items-center">
                 <Link href="/">
-                    <Home />
+                    <Home className="h-6 w-6" />
                 </Link>
                 {session?.user?.role === 'ADMIN'
-                    && <Link href="/admin">Admin panel</Link>
+                    && <Link href="/admin" className="text-white font-bold">Admin panel</Link>
                 }
-                <Link href="/dashboard">Dashboard</Link>
-                <Link href="/about">About</Link>
-                <Link href="/repartidores">Repartidores</Link>
-                <Link href="/pedidos">Pedidos</Link>
-                <Link href="/pizzas">Pizzas</Link>
+                <Link href="/dashboard" className="text-white font-bold">Dashboard</Link>
+                <Link href="/about" className="text-white font-bold">About</Link>
+                <Link href="/repartidores" className="text-white font-bold">Repartidores</Link>
+                <Link href="/pedidos" className="text-white font-bold">Pedidos</Link>
+                <Link href="/pizzas" className="text-white font-bold">Pizzas</Link>
             </nav>
-            <div className='flex gap-4'>
+            <div className="flex gap-4">
                 {session
-                    ? <form><button formAction={logout}>Logout</button></form>
-                    : <Link href="/auth/login">Login</Link>
+                    ? <form><button formAction={logout} className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Logout</button></form>
+                    : <Link href="/auth/login" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Login</Link>
                 }
             </div>
         </header>
